8fdaea7904944964780267af8d7c4065702432d8
abdulloh0512 <abdullohdev0512@gmail.com>
2024-05-31 01:58:41 +0500

    ok

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import HeaderSidebar from './components/HeaderSidebar'
+import HeroSidebar from './components/HeroSidebar'
+import LeftSidebar from './components/LeftSidebar'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full'>
+        <div className='flex'>
+          <LeftSidebar></LeftSidebar>
+          <div className='w-full'>
+            <HeaderSidebar></HeaderSidebar>
+            <HeroSidebar></HeroSidebar>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
